eb8ef8340179dd057170d680d88b674bea9a8abc
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2007-09-20 15:59:04 +0000

    méthode de test le trigger fonctionne désormais.

--- trunk/mmbv2/src/main/java/momobot/trigger/math/FractionContinueTrigger.java
+++ trunk/mmbv2/src/main/java/momobot/trigger/math/FractionContinueTrigger.java
@@ -4,7 +4,15 @@ import ircbot.Channel;
 import ircbot.IrcUser;
 import ircbot.trigger.AbstractTrigger;
 import ircbot.trigger.IPublicTrigger;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import momobot.MomoBot;
+
+import org.apache.commons.lang.text.StrBuilder;
+import org.apache.log4j.Logger;
 
 /**
  * En voilà un trigger qu'il est useless.
@@ -12,6 +20,82 @@ import momobot.MomoBot;
  * @author mauhiz
  */
 public class FractionContinueTrigger extends AbstractTrigger implements IPublicTrigger {
+    /**
+     * 
+     */
+    static final int            LIMIT = 10;
+    /**
+     * 
+     */
+    static final Logger LOG   = Logger.getLogger(FractionContinueTrigger.class);
+
+    /**
+     * TODO : un critère d'arrêt basé sur epsilon.
+     * 
+     * @param nombre
+     * @return une fraction continue de type [a0; a1, a2, ...]
+     */
+    static String computeFractionContinue(final double nombre) {
+        final List < Integer > fraction = new LinkedList < Integer >();
+        int floor;
+        double work = nombre;
+        // LOG.debug("limit = " + LIMIT);
+        int count = 0;
+        while (count < LIMIT) {
+            // LOG.debug("work = " + work);
+            floor = (int) Math.floor(work);
+            if (floor == Integer.MAX_VALUE) {
+                count = LIMIT + 1;
+                break;
+            }
+            // LOG.debug("floor = " + floor);
+            fraction.add(Integer.valueOf(floor));
+            work = work - floor;
+            // LOG.debug("work = " + work);
+            if (work == 0) {
+                count = LIMIT + 1;
+                break;
+            }
+            work = 1 / work;
+            ++count;
+        }
+        if (count > LIMIT) {
+            return displayFraction(fraction, true);
+        }
+        return displayFraction(fraction, false);
+    }
+
+    /**
+     * @param fraction
+     * @return l'affichage de la fraction continue;
+     */
+    static String displayFraction(final List < Integer > fraction, final boolean exactMatch) {
+        final List < Integer > safeCopy = new ArrayList < Integer >(fraction.size());
+        safeCopy.addAll(fraction);
+        final StrBuilder retour = new StrBuilder("[");
+        if (safeCopy.isEmpty()) {
+            retour.append("0]");
+        } else {
+            retour.append(safeCopy.remove(0));
+            if (safeCopy.isEmpty()) {
+                retour.append(']');
+            } else {
+                retour.append("; ");
+                for (final Integer fracItem : safeCopy) {
+                    retour.append(fracItem);
+                    retour.append(", ");
+                }
+                if (exactMatch) {
+                    retour.setLength(retour.length() - 2);
+                } else {
+                    retour.append("...");
+                }
+                retour.append(']');
+            }
+        }
+        return retour.toString();
+    }
+
     /**
      * @param trigger
      */
@@ -32,14 +116,5 @@ public class FractionContinueTrigger extends AbstractTrigger implements IPublicT
         } catch (NumberFormatException nfe) {
             MomoBot.getBotInstance().sendMessage(channel, "l'argument doit etre un nombre (reçu : " + args + ")");
         }
-    }
-
-    /**
-     * TODO .
-     * @param nombre
-     * @return une fraction continue de type [a0; a1, a2, ...]
-     */
-    private static String computeFractionContinue(final double nombre) {
-        return null;
     }
 }
