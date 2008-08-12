8e860b848719ebdeafc38dda496be79f5e1ab1bb
romain.alvergnat <romain.alvergnat@1e759161-072c-0410-a95b-f725d490f48e>
2008-08-12 20:52:24 +0000

    Mon deuxieme commit ! $shake devrait fonctionner

---
+++ momobot_3/src/test/java/net/mauhiz/irc/bot/triggers/event/gather/ShakeTest.java
@@ -0,0 +1,31 @@
+package net.mauhiz.irc.bot.triggers.event.gather;
+import org.junit.Test;
+
+/**
+ * @author mauhiz
+ * 
+ */
+public class ShakeTest {
+    
+    /**
+     * Test method for {@link net.mauhiz.irc.Morse#fromMorse(java.lang.String)}.
+     */
+    @Test
+    public void testFromMorse() {
+        // String morse = "... --- ...";
+        // String sos = Morse.fromMorse(morse);
+        // Assert.assertEquals("SOS", sos);
+        
+    }
+    
+    /**
+     * Test method for {@link net.mauhiz.irc.Morse#toMorse(java.lang.String)}.
+     */
+    @Test
+    public void testToMorse() {
+        // String sos = "SOS";
+        // String morse = Morse.toMorse(sos);
+        // Assert.assertEquals("...---...", morse);
+        
+    }
+}
