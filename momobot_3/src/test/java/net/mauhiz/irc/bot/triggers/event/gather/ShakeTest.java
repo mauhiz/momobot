ad45664036f0d52464a370295ba00e9c3fe8d30e
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2008-08-12 21:20:52 +0000

    exemple de test unitaire

--- momobot_3/src/test/java/net/mauhiz/irc/bot/triggers/event/gather/ShakeTest.java
+++ momobot_3/src/test/java/net/mauhiz/irc/bot/triggers/event/gather/ShakeTest.java
@@ -1,9 +1,30 @@
 package net.mauhiz.irc.bot.triggers.event.gather;
 
+import net.mauhiz.irc.base.data.Channel;
+import net.mauhiz.irc.base.data.IrcUser;
+import net.mauhiz.irc.bot.event.Pickup;
+
 /**
  * @author Topper
  * 
  */
 public class ShakeTest {
-    /* TODO */
+    
+    /**
+     * exemple de test
+     */
+    public void testShake() {
+        Channel chan = new Channel("#tsi.fr");
+        Pickup p = new Pickup(chan);
+        IrcUser a = new IrcUser("a");
+        IrcUser b = new IrcUser("b");
+        IrcUser c = new IrcUser("c");
+        IrcUser d = new IrcUser("d");
+        p.add(a, "a");
+        p.add(b, "a");
+        p.add(c, "a");
+        p.add(d, "a");
+        p.shake();
+        System.out.println(p);
+    }
 }
