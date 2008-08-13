b3ff07b566cc0661206bc81ac9209887f9025bc1
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2008-08-13 15:42:08 +0000

    maj usermap

--- momobot_3/src/test/java/net/mauhiz/irc/base/model/UsersTest.java
+++ momobot_3/src/test/java/net/mauhiz/irc/base/model/UsersTest.java
@@ -8,7 +8,6 @@ import java.net.URISyntaxException;
 import net.mauhiz.irc.base.data.Channel;
 import net.mauhiz.irc.base.data.IrcServer;
 import net.mauhiz.irc.base.data.IrcUser;
-import net.mauhiz.irc.base.model.Users;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -30,7 +29,7 @@ public class UsersTest {
         qnet.setAlias("Quakenet");
         qnet.setMyNick("momobot3");
     }
-
+    
     /**
      * @throws java.lang.Exception
      */
@@ -38,36 +37,35 @@ public class UsersTest {
     public void setUp() throws Exception {
         Users.get(qnet);
     }
-
+    
     /**
-     * Test method for {@link net.mauhiz.irc.base.model.Users#getChannels(net.mauhiz.irc.base.data.IrcUser)}.
+     * Test method for {@link net.mauhiz.irc.base.model.Channels#getChannels(net.mauhiz.irc.base.data.IrcUser)}.
      */
     @Test
     public void testMultiChan() {
-        Channel chan = new Channel("#tsi.fr");
-        IrcUser peon = new IrcUser("Truite");
-        IrcUser peon2 = new IrcUser("Gruiiik");
-        Users.get(qnet).put(chan, peon);
-        Users.get(qnet).put(chan, peon2);
-        Channel chan2 = new Channel("#-duCk-");
-        Users.get(qnet).put(chan2, peon2);
-        Assert.assertTrue(2 == Users.get(qnet).getChannels(peon2).size());
-        Assert.assertTrue(2 == Users.get(qnet).countUsers());
+        Channel chan = Channels.get(qnet).buildChannel("#tsi.Fr");
+        IrcUser peon = Users.get(qnet).findUser("Truite", true);
+        IrcUser peon2 = Users.get(qnet).findUser("Gruiiik", true);
+        chan.add(peon);
+        chan.add(peon2);
+        Channel chan2 = Channels.get(qnet).buildChannel("#-duCk-");
+        chan2.add(peon2);
+        Assert.assertEquals(2, Channels.get(qnet).getChannels(peon2).size());
+        Assert.assertEquals(2, Users.get(qnet).size());
     }
-
+    
     /**
-     * Test method for
-     * {@link net.mauhiz.irc.base.model.Users#put(net.mauhiz.irc.base.data.Channel, net.mauhiz.irc.base.data.IrcUser)}.
+     * Test method for {@link net.mauhiz.irc.base.model.Users#isKnown(IrcUser)}
      */
     @Test
     public void testSingleChan() {
-        Channel chan = new Channel("#tsi.fr");
-        IrcUser peon = new IrcUser("Truite");
-        IrcUser peon2 = new IrcUser("Gruiiik");
-        Users.get(qnet).put(chan, peon);
-        Users.get(qnet).put(chan, peon2);
+        Channel chan = Channels.get(qnet).buildChannel("#tsi.Fr");
+        IrcUser peon = Users.get(qnet).findUser("Truite", true);
+        IrcUser peon2 = Users.get(qnet).findUser("Gruiiik", true);
+        chan.add(peon);
+        chan.add(peon2);
         Assert.assertTrue(Users.get(qnet).isKnown(peon));
         Assert.assertTrue(Users.get(qnet).isKnown(peon2));
-        Assert.assertTrue(2 == Users.get(qnet).getUsers(chan).size());
+        Assert.assertEquals(2, chan.size());
     }
 }
