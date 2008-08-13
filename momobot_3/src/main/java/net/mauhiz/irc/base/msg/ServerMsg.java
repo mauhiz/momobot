e1ef997af2f0931d8286f82348ead99cfe4439c7
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2008-08-13 14:09:48 +0000

    getters

--- momobot_3/src/main/java/net/mauhiz/irc/base/msg/ServerMsg.java
+++ momobot_3/src/main/java/net/mauhiz/irc/base/msg/ServerMsg.java
@@ -5,10 +5,16 @@ import net.mauhiz.irc.base.data.IrcServer;
 /**
  * @author mauhiz
  */
-public class ServerMsg extends IrcMessage {
-    int    code;
-    String msg;
-
+public class ServerMsg extends IrcMessage implements NumericReplies {
+    /**
+     * code numerique
+     */
+    private int code;
+    /**
+     * suite du message
+     */
+    private String msg;
+    
     /**
      * @param from1
      * @param to1
@@ -17,26 +23,40 @@ public class ServerMsg extends IrcMessage {
     public ServerMsg(final String from1, final String to1, final IrcServer server1) {
         super(from1, to1, server1);
     }
-
+    
     /**
-     * @param object
-     * @param object2
+     * @param from1
+     * @param to1
      * @param ircServer
-     * @param group
+     * @param codeStr
      * @param group2
      */
-    public ServerMsg(final String object, final String object2, final IrcServer ircServer, final String group,
+    public ServerMsg(final String from1, final String to1, final IrcServer ircServer, final String codeStr,
             final String group2) {
-        this(object, object2, ircServer);
-        this.code = Integer.parseInt(group);
-        this.msg = group2;
+        this(from1, to1, ircServer);
+        code = Integer.parseInt(codeStr);
+        msg = group2;
     }
-
+    
+    /**
+     * @return {@link #code}
+     */
+    public int getCode() {
+        return code;
+    }
+    
+    /**
+     * @return {@link #msg}
+     */
+    public String getMsg() {
+        return msg;
+    }
+    
     /**
      * @see java.lang.Object#toString()
      */
     @Override
     public String toString() {
-        return "server msg: code=" + this.code + " msg=" + this.msg;
+        return "server msg: code=" + code + " msg=" + msg;
     }
 }
