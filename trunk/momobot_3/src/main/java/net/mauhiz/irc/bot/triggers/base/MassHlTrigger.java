8c0842125f1018cd92e87599c225078b18af787c
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2008-08-13 15:01:39 +0000

    utilisation de l user map

--- trunk/momobot_3/src/main/java/net/mauhiz/irc/bot/triggers/base/MassHlTrigger.java
+++ trunk/momobot_3/src/main/java/net/mauhiz/irc/bot/triggers/base/MassHlTrigger.java
@@ -1,14 +1,11 @@
 package net.mauhiz.irc.bot.triggers.base;
 
-import java.util.Set;
-
 import net.mauhiz.irc.base.IIrcControl;
 import net.mauhiz.irc.base.data.Channel;
 import net.mauhiz.irc.base.data.IrcServer;
 import net.mauhiz.irc.base.data.IrcUser;
 import net.mauhiz.irc.base.data.qnet.QnetUser;
 import net.mauhiz.irc.base.model.Channels;
-import net.mauhiz.irc.base.model.Users;
 import net.mauhiz.irc.base.msg.Privmsg;
 import net.mauhiz.irc.bot.triggers.AbstractTextTrigger;
 import net.mauhiz.irc.bot.triggers.IPrivmsgTrigger;
@@ -42,15 +39,14 @@ public class MassHlTrigger extends AbstractTextTrigger implements IPrivmsgTrigge
     public void doTrigger(final Privmsg cme, final IIrcControl control) {
         IrcServer server = cme.getServer();
         Channel chan = Channels.get(server).getChannel(cme.getTo());
-        Set<IrcUser> users = Users.get(server).getUsers(chan);
-        int nbUsers = users.size();
-        LOG.debug("MassHlTrigger : " + chan + " has " + nbUsers + " users");
-        if (nbUsers == 0) {
+        if (chan.isEmpty()) {
             LOG.error("no user on channel " + cme.getTo());
             return;
         }
+        
+        LOG.debug("MassHlTrigger : " + chan + " has " + chan.size() + " users");
         final StrBuilder msg = new StrBuilder("nudges");
-        for (final IrcUser nextIrcUser : users) {
+        for (final IrcUser nextIrcUser : chan) {
             if (nextIrcUser instanceof QnetUser && ((QnetUser) nextIrcUser).isService()) {
                 /* no bots */
                 LOG.debug("skipping bot : " + nextIrcUser);
