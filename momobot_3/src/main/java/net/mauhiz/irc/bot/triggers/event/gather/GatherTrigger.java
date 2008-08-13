833c7a3bc4feb1f849979c27b1d0190bb47b5106
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2008-08-13 13:39:10 +0000

    dirty fix pour le gather

--- momobot_3/src/main/java/net/mauhiz/irc/bot/triggers/event/gather/GatherTrigger.java
+++ momobot_3/src/main/java/net/mauhiz/irc/bot/triggers/event/gather/GatherTrigger.java
@@ -4,8 +4,8 @@ import net.mauhiz.irc.base.IIrcControl;
 import net.mauhiz.irc.base.data.Channel;
 import net.mauhiz.irc.base.data.IrcServer;
 import net.mauhiz.irc.base.data.IrcUser;
+import net.mauhiz.irc.base.data.Mask;
 import net.mauhiz.irc.base.model.Channels;
-import net.mauhiz.irc.base.model.Users;
 import net.mauhiz.irc.base.msg.Privmsg;
 import net.mauhiz.irc.bot.event.ChannelEvent;
 import net.mauhiz.irc.bot.event.Gather;
@@ -37,7 +37,9 @@ public class GatherTrigger extends AbstractTextTrigger implements IPrivmsgTrigge
             respMsg = "Un " + evt.getClass().getSimpleName() + " est déjà lancé sur " + cme.getTo();
         } else {
             respMsg = "Gather lancé par " + cme.getFrom();
-            IrcUser user = Users.get(server).findUser(cme.getFrom());
+            /* FIXME use User Map */
+            // IrcUser user = Users.get(server).findUser(cme.getFrom());
+            IrcUser user = new IrcUser(new Mask(cme.getFrom()));
             new Gather(chan).add(user);
         }
         Privmsg resp = Privmsg.buildAnswer(cme, respMsg);
