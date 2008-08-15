60f77302744b179bc11664045a32fc15a8038d91
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2008-08-15 19:13:25 +0000

    requested msg

--- trunk/momobot_3/src/main/java/net/mauhiz/irc/bot/triggers/admin/QuitTrigger.java
+++ trunk/momobot_3/src/main/java/net/mauhiz/irc/bot/triggers/admin/QuitTrigger.java
@@ -1,6 +1,9 @@
 package net.mauhiz.irc.bot.triggers.admin;
 
 import net.mauhiz.irc.base.IIrcControl;
+import net.mauhiz.irc.base.data.IrcUser;
+import net.mauhiz.irc.base.data.Mask;
+import net.mauhiz.irc.base.model.Users;
 import net.mauhiz.irc.base.msg.Privmsg;
 import net.mauhiz.irc.base.msg.Quit;
 import net.mauhiz.irc.bot.triggers.AbstractTextTrigger;
@@ -31,7 +34,8 @@ public class QuitTrigger extends AbstractTextTrigger implements IPrivmsgTrigger,
         /* TODO quit cross servers */
         Quit quit;
         if (StringUtils.isEmpty(args)) {
-            quit = new Quit(im.getServer(), "Requested by " + im.getFrom());
+            IrcUser from = Users.get(im.getServer()).findUser(new Mask(im.getFrom()), true);
+            quit = new Quit(im.getServer(), "Requested by " + from);
         } else {
             quit = new Quit(im.getServer(), args);
         }
