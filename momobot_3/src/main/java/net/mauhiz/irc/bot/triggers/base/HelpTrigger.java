850a4645890791f96e3a8a8b9bd995467dae0e8e
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2008-08-15 15:55:53 +0000

    refactoring des triggers

--- momobot_3/src/main/java/net/mauhiz/irc/bot/triggers/base/HelpTrigger.java
+++ momobot_3/src/main/java/net/mauhiz/irc/bot/triggers/base/HelpTrigger.java
@@ -1,11 +1,12 @@
 package net.mauhiz.irc.bot.triggers.base;
 
+import java.util.Set;
+
 import net.mauhiz.irc.base.IIrcControl;
 import net.mauhiz.irc.base.IrcControl;
 import net.mauhiz.irc.base.msg.Notice;
 import net.mauhiz.irc.base.msg.Privmsg;
 import net.mauhiz.irc.bot.MmbTriggerManager;
-import net.mauhiz.irc.bot.TriggerKeeper;
 import net.mauhiz.irc.bot.triggers.AbstractTextTrigger;
 import net.mauhiz.irc.bot.triggers.INoticeTrigger;
 import net.mauhiz.irc.bot.triggers.IPrivmsgTrigger;
@@ -21,12 +22,6 @@ public class HelpTrigger extends AbstractTextTrigger implements IPrivmsgTrigger,
      * Commandes :.
      */
     private static final String COMMANDES = "Commandes :";
-    private TriggerKeeper keeper;
-    
-    /**
-     * message.
-     */
-    private final StrBuilder msg = new StrBuilder();
     
     /**
      * @param trigger
@@ -42,11 +37,9 @@ public class HelpTrigger extends AbstractTextTrigger implements IPrivmsgTrigger,
      */
     @Override
     public void doTrigger(final Notice im, final IIrcControl control) {
-        if (keeper == null) {
-            initKeeper(control);
-        }
-        msg.clear().append(COMMANDES);
-        for (final ITrigger trig : keeper) {
+        StrBuilder msg = new StrBuilder();
+        msg.append(COMMANDES);
+        for (final ITrigger trig : getTriggers(control)) {
             if (trig instanceof INoticeTrigger) {
                 msg.append(trig).append(' ');
             }
@@ -55,18 +48,15 @@ public class HelpTrigger extends AbstractTextTrigger implements IPrivmsgTrigger,
         control.sendMsg(resp);
         
     }
-    
     /**
      * @see net.mauhiz.irc.bot.triggers.IPrivmsgTrigger#doTrigger(net.mauhiz.irc.base.msg.Privmsg,
      *      net.mauhiz.irc.base.IIrcControl)
      */
     @Override
     public void doTrigger(final Privmsg im, final IIrcControl control) {
-        if (keeper == null) {
-            initKeeper(control);
-        }
-        msg.clear().append(COMMANDES);
-        for (final ITrigger trig : keeper) {
+        StrBuilder msg = new StrBuilder();
+        msg.append(COMMANDES);
+        for (final ITrigger trig : getTriggers(control)) {
             if (trig instanceof IPrivmsgTrigger) {
                 msg.append(trig).append(' ');
             }
@@ -77,10 +67,11 @@ public class HelpTrigger extends AbstractTextTrigger implements IPrivmsgTrigger,
     
     /**
      * @param control
+     * @return triggers view
      */
-    private void initKeeper(final IIrcControl control) {
+    private Set<ITrigger> getTriggers(final IIrcControl control) {
         IrcControl realControl = (IrcControl) control;
         MmbTriggerManager manager = (MmbTriggerManager) realControl.getManager();
-        keeper = manager.getKeeper();
+        return manager.getTriggers();
     }
 }
