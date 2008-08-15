be17c4724b987cc82bcdf71b62a84d471e3a5d8f
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2008-08-15 10:57:10 +0000

    method remove

--- trunk/momobot_3/src/main/java/net/mauhiz/irc/bot/TriggerKeeper.java
+++ trunk/momobot_3/src/main/java/net/mauhiz/irc/bot/TriggerKeeper.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import net.mauhiz.irc.bot.triggers.AbstractTextTrigger;
 import net.mauhiz.irc.bot.triggers.ITrigger;
 
+import org.apache.commons.lang.ArrayUtils;
+
 /**
  * @author mauhiz
  */
@@ -33,5 +35,30 @@ public class TriggerKeeper extends HashSet<ITrigger> {
             }
         }
         return super.add(e);
+    }
+    
+    /**
+     * @param toRemove
+     * @param texts
+     * @return {@link #remove(Object)}
+     */
+    public boolean remove(final Class<? extends ITrigger> toRemove, final String[] texts) {
+        for (ITrigger every : this) {
+            if (toRemove.equals(every.getClass())) {
+                if (every instanceof AbstractTextTrigger && !ArrayUtils.isEmpty(texts)) {
+                    AbstractTextTrigger textTrigger = (AbstractTextTrigger) every;
+                    boolean atLeastOne = false;
+                    for (String text : texts) {
+                        if (textTrigger.getTriggerText().equals(text)) {
+                            atLeastOne |= super.remove(every);
+                        }
+                    }
+                    return atLeastOne;
+                }
+                /* degage */
+                return super.remove(every);
+            }
+        }
+        return false;
     }
 }
