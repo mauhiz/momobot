ade7c97c2721b1e183c249949cdbb184fd87407b
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2008-08-16 19:02:10 +0000

    1st commit

---
+++ momobot_3/src/test/java/net/mauhiz/irc/bot/triggers/websearch/BashTriggerTest.java
@@ -0,0 +1,29 @@
+package net.mauhiz.irc.bot.triggers.websearch;
+
+import org.junit.Test;
+
+/**
+ * @author mauhiz
+ * 
+ */
+public class BashTriggerTest {
+    
+    /**
+     * Test method for {@link net.mauhiz.irc.bot.triggers.websearch.BashTrigger#getNextFrQuote()}.
+     */
+    @Test
+    public final void testGetNextFrQuote() {
+        String[] lignes = BashTrigger.getNextFrQuote();
+        for (String ligne : lignes) {
+            System.out.println(ligne);
+        }
+    }
+    
+    /**
+     * Test method for {@link net.mauhiz.irc.bot.triggers.websearch.BashTrigger#getNextQuote()}.
+     */
+    @Test
+    public final void testGetNextQuote() {
+        // System.out.println(BashTrigger.getNextQuote());
+    }
+}
