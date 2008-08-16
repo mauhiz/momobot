e1bbb6de83a2f138a7a69b590ec03c6c0d1127ac
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2008-08-16 20:12:12 +0000

    reponse en privmsg

--- momobot_3/src/main/java/net/mauhiz/irc/bot/triggers/websearch/GoogleTrigger.java
+++ momobot_3/src/main/java/net/mauhiz/irc/bot/triggers/websearch/GoogleTrigger.java
@@ -1,7 +1,7 @@
 package net.mauhiz.irc.bot.triggers.websearch;
 
 import net.mauhiz.irc.base.IIrcControl;
-import net.mauhiz.irc.base.msg.Notice;
+import net.mauhiz.irc.base.msg.IIrcMessage;
 import net.mauhiz.irc.base.msg.Privmsg;
 import net.mauhiz.irc.bot.triggers.AbstractTextTrigger;
 import net.mauhiz.irc.bot.triggers.IPrivmsgTrigger;
@@ -26,7 +26,7 @@ public class GoogleTrigger extends AbstractTextTrigger implements IPrivmsgTrigge
     public void doTrigger(final Privmsg cme, final IIrcControl control) {
         final WebQuery query = new WebQuery("google", getArgs(cme.getMessage()));
         for (final String next : query.results()) {
-            Notice notice = Notice.buildPrivateAnswer(cme, next);
+            IIrcMessage notice = Privmsg.buildAnswer(cme, next);
             control.sendMsg(notice);
         }
     }
