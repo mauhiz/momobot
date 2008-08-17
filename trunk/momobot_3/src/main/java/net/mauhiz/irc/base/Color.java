989b916e764a222da7173c69bb652f4649b9e352
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2008-08-17 14:38:35 +0000

    bug couleurs

--- trunk/momobot_3/src/main/java/net/mauhiz/irc/base/Color.java
+++ trunk/momobot_3/src/main/java/net/mauhiz/irc/base/Color.java
@@ -4,26 +4,21 @@ import org.apache.commons.lang.StringUtils;
 
 /**
  * @author mauhiz
- * 
  */
 public enum Color {
-    BLACK(1), BLUE(12), BROWN(5), CYAN(11), DARK_BLUE(2), DARK_GRAY(14), DARK_GREEN(3), GREEN(9), LIGHT_GRAY(15), MAGENTA(
-            13), OLIVE(7), PURPLE(6), RED(4), TEAL(10), WHITE(0), YELLOW(8);
+    BLACK(0x1), BLUE(0xc), BROWN(0x5), CYAN(0xb), DARK_BLUE(0x2), DARK_GRAY(0xe), DARK_GREEN(0x3), GREEN(0x9), LIGHT_GRAY(
+            0xf), MAGENTA(0xd), OLIVE(0x7), PURPLE(0x6), RED(0x4), TEAL(0xa), WHITE(0x0), YELLOW(0x8);
     
+    /**
+     * code couleur mIRC.
+     */
     private String code;
     
     /**
      * @param intCode
      */
     private Color(final int intCode) {
-        code = StringUtils.leftPad(Integer.toString(intCode), 2);
-    }
-    
-    /**
-     * @return {@link #code}
-     */
-    public String getCode() {
-        return code;
+        code = StringUtils.leftPad(Integer.toString(intCode), 2, '0');
     }
     
     /**
