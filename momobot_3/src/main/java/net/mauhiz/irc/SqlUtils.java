993f8ad310203b231f8c9adb9cfd0f3e360c097e
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2008-09-07 09:42:58 +0000

    nettoyage

--- momobot_3/src/main/java/net/mauhiz/irc/SqlUtils.java
+++ momobot_3/src/main/java/net/mauhiz/irc/SqlUtils.java
@@ -1,21 +1,15 @@
 package net.mauhiz.irc;
 
-import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.Map.Entry;
 
-import net.mauhiz.irc.bot.triggers.dispo.Dispo;
-
-import org.apache.commons.beanutils.ConstructorUtils;
 import org.apache.log4j.Logger;
 
 /**
@@ -97,34 +91,6 @@ public class SqlUtils {
     }
     
     /**
-     * @param channel
-     *            le channel
-     * @param date
-     *            la date
-     * @return la map
-     * @throws SQLException
-     *             en cas d'erreur.
-     */
-    public static Iterable<Dispo> getDispos(final String channel, final long date) throws SQLException {
-        final Date datesql = new Date(date);
-        final ResultSet rs = doSqlQuery("SELECT `qauth`, `heure1`, `heure2` FROM `dispo` WHERE `channel` = '" + channel
-                + "' AND `date`= '" + datesql.toString() + "'");
-        final Collection<Dispo> dispos = new HashSet<Dispo>();
-        try {
-            while (rs.next()) {
-                final String qauth = rs.getString("qauth");
-                final int heure1 = rs.getInt("heure1");
-                final int heure2 = rs.getInt("heure2");
-                final Dispo dispo = new Dispo(qauth, heure1, heure2);
-                dispos.add(dispo);
-            }
-        } finally {
-            rs.close();
-        }
-        return dispos;
-    }
-    
-    /**
      * @return les players
      */
     public static Iterable<Entry<String, String>> getPlayers() {
@@ -163,27 +129,6 @@ public class SqlUtils {
     }
     
     /**
-     * @param className
-     * @param triggerText
-     */
-    static void loadTrigger(final String className, final String triggerText) {
-        try {
-            final Class<?> toLoad = Class.forName(className);
-            ConstructorUtils.invokeConstructor(toLoad, triggerText);
-        } catch (final NoSuchMethodException nsme) {
-            LOG.error("Invalid trigger class: " + className, nsme);
-        } catch (final ClassNotFoundException cnfe) {
-            LOG.warn("Trigger not found: " + className, cnfe);
-        } catch (final InstantiationException ie) {
-            LOG.error("Could not create trigger: " + className, ie);
-        } catch (final IllegalAccessException iae) {
-            LOG.error("Invalid trigger class: " + className, iae);
-        } catch (final InvocationTargetException ite) {
-            LOG.error("Could not create trigger: " + className, ite.getTargetException());
-        }
-    }
-    
-    /**
      * @param steamid
      *            le steam_id
      * @param qnetAuth
@@ -193,7 +138,7 @@ public class SqlUtils {
     public static String registerPlayer(final String steamid, final String qnetAuth) {
         PLAYERS.put(steamid, qnetAuth);
         try {
-            doSqlUpdate("INSERT INTO `players`(`steamid`, `qauth`) VALUES('" + steamid + "', '" + qnetAuth + "')");
+            doSqlUpdate("INSERT INTO players (steamid, qauth) VALUES ('" + steamid + "', '" + qnetAuth + "')");
             return "Joueur enregistré";
         } catch (final SQLException sqle) {
             LOG.error(sqle, sqle);
@@ -208,29 +153,6 @@ public class SqlUtils {
      */
     public static boolean steamidIsKnown(final String steamid) {
         return PLAYERS.containsKey(steamid);
-    }
-    
-    /**
-     * @param channel
-     *            le channel
-     * @param qauth
-     *            l'auth Qnet
-     * @param time
-     *            La date
-     * @param heure1
-     *            indicateur
-     * @param heure2
-     *            indicateur
-     * @throws SQLException
-     *             en cas de galère.
-     */
-    public static void updateDispo(final String channel, final String qauth, final long time, final byte heure1,
-            final byte heure2) throws SQLException {
-        final String date = new java.sql.Date(time).toString();
-        doSqlUpdate("DELETE FROM `dispo` WHERE `channel` = '" + channel + "' AND `qauth` = '" + qauth
-                + "' AND `date` = '" + date + "'");
-        doSqlUpdate("INSERT INTO `dispo`(`channel`, `qauth`, `date`, `heure1`, `heure2`) VALUES('" + channel + "', '"
-                + qauth + "', '" + date + "', " + heure1 + ", " + heure2 + ")");
     }
     
     /**
