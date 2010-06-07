fddfc02eff2256a55204c30c860563b9e69ab382
jerome.pericart <jerome.pericart@1e759161-072c-0410-a95b-f725d490f48e>
2010-06-07 20:46:04 +0000

    Ajout d'un classe de test unitaire sur seekwar2, début de correction de la classe SeekWar2 (il y a du ménage à faire, et les premiers tests unitaires ne sont pas tous satisfaits)
    Pourquoi la classe s'appelle SeekWar2 au lieu de SeekWar ?

---
+++ momobot_3/src/test/java/net/mauhiz/irc/bot/event/SeekWar2Test.java
@@ -0,0 +1,49 @@
+package net.mauhiz.irc.bot.event;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Abby
+ * 
+ */
+
+public class SeekWar2Test {
+    /**
+     * 
+     */
+    @Test
+    public void isMatchAgreeMessageTest() {
+        Assert.assertTrue(SeekWar2.isMatchAgreeMessage("k"));
+        
+        Assert.assertTrue(SeekWar2.isMatchAgreeMessage("go"));
+        
+        Assert.assertFalse(SeekWar2.isMatchAgreeMessage("tg"));
+    }
+    
+    @Test
+    public void isMatchIpTest() {
+        
+        // Exemple classique
+        Assert.assertTrue(SeekWar2.isMatchIp("192.168.0.15:27015"));
+        
+        // Avec 5 chiffres aux lieux de 4
+        Assert.assertFalse(SeekWar2.isMatchIp("192.168.0.15.1:27015"));
+        
+        // Avec 3 chiffres au lieux de 3
+        Assert.assertFalse(SeekWar2.isMatchIp("192.1680.15:27015"));
+        
+        // Avec un caractère alphabétique
+        Assert.assertFalse(SeekWar2.isMatchIp("192.168.a.15:27015"));
+        
+    }
+    
+    @Test
+    public void isMatchUrlTest() {
+        Assert.assertTrue(SeekWar2.isMatchUrl("www.mauhiz.net"));
+        
+        Assert.assertFalse(SeekWar2.isMatchUrl("wrong"));
+        
+    }
+    
+}
