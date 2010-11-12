d165c25cf570a958eb18f199f7ac6f398005e597
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2010-11-12 04:20:05 +0000

    server test is mainly QNET

--- trunk/momobot_3/src/test/java/net/mauhiz/irc/AbstractServerTest.java
+++ trunk/momobot_3/src/test/java/net/mauhiz/irc/AbstractServerTest.java
@@ -1,17 +1,21 @@
 package net.mauhiz.irc;
 
-import net.mauhiz.irc.base.data.IrcServer;
-import net.mauhiz.irc.base.data.IrcUser;
 import net.mauhiz.irc.base.data.qnet.QnetServer;
+import net.mauhiz.irc.base.data.qnet.QnetUser;
 
+/**
+ * Effectue les tests avec un serveur type Qnet
+ * 
+ * @author mauhiz
+ */
 public class AbstractServerTest {
     
-    protected static final IrcServer QNET = new QnetServer("irc://uk.quakenet.org:6667/");
+    protected static final QnetServer QNET = new QnetServer("irc://uk.quakenet.org:6667/");
     
     static {
         QNET.setAlias("QuakeNet");
         
-        IrcUser myself = QNET.newUser("momobot3");
+        QnetUser myself = QNET.newUser("momobot3");
         myself.setUser("mmb");
         myself.setFullName("MMB v3");
         QNET.setMyself(myself);
