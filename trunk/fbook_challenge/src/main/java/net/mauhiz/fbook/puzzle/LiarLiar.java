68fd3eaa9cd5feb23e10fccc4ff4a3bb5f60b030
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2011-01-27 02:05:50 +0000

    bzzt

--- trunk/fbook_challenge/src/main/java/net/mauhiz/fbook/puzzle/LiarLiar.java
+++ trunk/fbook_challenge/src/main/java/net/mauhiz/fbook/puzzle/LiarLiar.java
@@ -3,6 +3,11 @@ package net.mauhiz.fbook.puzzle;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class LiarLiar extends ExportedRunnable {
 
@@ -14,11 +19,58 @@ public class LiarLiar extends ExportedRunnable {
 	public String getName() {
 		return "liarliar";
 	}
+	
+	static class Veteran {
+		String name;
+		final List<Veteran> accusers = new ArrayList<Veteran>();
+		Boolean liar;
+		static Veteran instance(Map<String, Veteran> members, String name) {
+			Veteran v = members.get(name);
+			if (v == null) {
+				v = new Veteran();
+				v.name = name;
+				members.put(name, v);
+			}
+			return v;
+		}
+	}
+	
+
+	static int count(boolean liars, Collection<Veteran> members) {
+		int c = 0;
+		for (Veteran v : members) {
+			if (v.liar == null) {
+				return -1;
+			} else if (v.liar.booleanValue() == liars) {
+				c++;
+			}
+		}
+		
+		return c;
+	}
 
 	@Override
 	protected void process(BufferedReader input, PrintWriter output) throws IOException {
-		int m = Integer.parseInt(input.readLine());
-		output.println("");
+		int n = Integer.parseInt(input.readLine());
+		Map<String, Veteran> members = new HashMap<String, Veteran>(n);
+		for (int i = 0; i < n; i++) {
+			String[] accusation = input.readLine().split(" ");
+			String accusedName = accusation[0];
+			Veteran accused = Veteran.instance(members, accusedName);
+
+			int m = Integer.parseInt(accusation[1]);
+			for (int j = 0; j < m; j++) {
+				String accuserName = input.readLine();
+				Veteran accuser = Veteran.instance(members, accuserName);
+				accused.accusers.add(accuser);
+			}
+		}
+		
+		// TODO bipartite graph analysis
+		
+		int countLiars = count(true, members.values());
+		int countHonest = count(false, members.values());
+		output.println(Math.max(countLiars, countHonest) + " " + Math.min(countLiars, countHonest));
 	}
 
 }
