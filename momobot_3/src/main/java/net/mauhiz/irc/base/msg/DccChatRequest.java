255f8702044a1bded2d5dcc706190941eb989225
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2011-02-23 05:04:04 +0000

    added decoding DCC messages

--- momobot_3/src/main/java/net/mauhiz/irc/base/msg/DccChatRequest.java
+++ momobot_3/src/main/java/net/mauhiz/irc/base/msg/DccChatRequest.java
@@ -5,6 +5,8 @@ import java.net.InetAddress;
 import net.mauhiz.irc.base.data.IrcServer;
 import net.mauhiz.util.NetUtils;
 
+import org.apache.commons.lang.StringUtils;
+
 /**
  * @author mauhiz
  */
@@ -16,9 +18,17 @@ public class DccChatRequest extends Ctcp {
     /**
      */
     public DccChatRequest(String from1, String to1, IrcServer server1, InetAddress address, int port) {
-        super(from1, to1, server1, "CHAT " + NetUtils.iaToLong(address) + " " + port);
+        super(from1, to1, server1, "CHAT CHAT " + NetUtils.iaToLong(address) + " " + port);
         this.address = address;
         this.port = port;
+    }
+
+    public DccChatRequest(String from, String to, IrcServer server, String ctcpContent) {
+        super(from, to, server, ctcpContent);
+        String[] tokens = StringUtils.split(ctcpContent, ' ');
+        long addrLong = Long.parseLong(tokens[2]);
+        address = NetUtils.longToIa(addrLong);
+        port = Integer.parseInt(tokens[3]);
     }
 
     public InetAddress getAddress() {
@@ -27,7 +37,7 @@ public class DccChatRequest extends Ctcp {
 
     @Override
     protected String getCommand() {
-        return "DCC CHAT";
+        return DCC;
     }
 
     public int getPort() {
