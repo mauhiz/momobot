7e68ef0fb9029eea78539c557c25dee183b67945
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2011-04-08 05:52:51 +0000

    added asynchronicity

--- momobot_3/src/main/java/net/mauhiz/irc/gui/actions/JoinAction.java
+++ momobot_3/src/main/java/net/mauhiz/irc/gui/actions/JoinAction.java
@@ -1,32 +1,49 @@
 package net.mauhiz.irc.gui.actions;
 
+import net.mauhiz.irc.base.IIrcControl;
 import net.mauhiz.irc.base.data.IrcServer;
 import net.mauhiz.irc.base.msg.Join;
+import net.mauhiz.irc.gui.ChannelUpdateTrigger;
 import net.mauhiz.irc.gui.GuiTriggerManager;
 import net.mauhiz.util.AbstractAction;
+
+import org.eclipse.swt.widgets.List;
 
 /**
  * @author mauhiz
  */
 public class JoinAction extends AbstractAction {
     String channel;
+    ChannelUpdateTrigger cut;
     GuiTriggerManager gtm;
     IrcServer server;
+    List userList;
 
     /**
      * @param gtm1
      * @param server1
      * @param channel1
      */
-    public JoinAction(GuiTriggerManager gtm1, IrcServer server1, String channel1) {
+    public JoinAction(GuiTriggerManager gtm1, IrcServer server1, String channel1, ChannelUpdateTrigger cut,
+            List userList) {
         server = server1;
         gtm = gtm1;
         channel = channel1;
+        this.cut = cut;
+        this.userList = userList;
     }
 
     @Override
     protected void doAction() {
         Join msg = new Join(server, channel);
-        gtm.getClient().sendMsg(msg);
+        IIrcControl control = gtm.getClient();
+        assert control != null;
+        control.sendMsg(msg);
+        cut.addChannel(server.findChannel(channel, true), userList);
+    }
+
+    @Override
+    protected boolean isAsynchronous() {
+        return false; // sending the message should be quick
     }
 }
--- momobot_3/src/main/java/net/mauhiz/util/AbstractAction.java
+++ momobot_3/src/main/java/net/mauhiz/util/AbstractAction.java
@@ -2,7 +2,11 @@ package net.mauhiz.util;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
 
+import javax.swing.SwingUtilities;
+
+import org.apache.log4j.Logger;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 
@@ -11,13 +15,33 @@ import org.eclipse.swt.events.SelectionListener;
  * @author mauhiz
  */
 public abstract class AbstractAction implements SelectionListener, ActionListener {
+    class ActionRunnable implements Runnable {
+        @Override
+        public void run() {
+            doAction();
+        }
+    }
+
+    protected static final Logger LOG = Logger.getLogger(AbstractAction.class);
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        doAction();
+        if (isAsynchronous()) {
+            SwingUtilities.invokeLater(new ActionRunnable());
+        } else {
+            try {
+                SwingUtilities.invokeAndWait(new ActionRunnable());
+            } catch (InterruptedException ie) {
+                AbstractRunnable.handleInterruption(ie);
+            } catch (InvocationTargetException ite) {
+                LOG.error(ite.getTargetException(), ite);
+            }
+        }
     }
 
     protected abstract void doAction();
+
+    protected abstract boolean isAsynchronous();
 
     @Override
     public void widgetDefaultSelected(SelectionEvent arg0) {
@@ -26,7 +50,11 @@ public abstract class AbstractAction implements SelectionListener, ActionListene
 
     @Override
     public void widgetSelected(SelectionEvent arg0) {
-        doAction();
+        if (isAsynchronous()) {
+            arg0.display.asyncExec(new ActionRunnable());
+        } else {
+            arg0.display.syncExec(new ActionRunnable());
+        }
     }
 
 }
