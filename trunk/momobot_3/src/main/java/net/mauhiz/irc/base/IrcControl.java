10a5f39517d2442acde6f0cb927fccfd9855d48d
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2011-04-08 06:56:21 +0000

    process more modes assert wherever possible

--- trunk/momobot_3/src/main/java/net/mauhiz/irc/base/IrcControl.java
+++ trunk/momobot_3/src/main/java/net/mauhiz/irc/base/IrcControl.java
@@ -26,19 +26,19 @@ public class IrcControl extends AbstractIrcControl implements IIrcClientControl 
      * logger.
      */
     private static final Logger LOG = Logger.getLogger(IrcControl.class);
-    
+
     /**
      * key = {@link IrcServer}, value = {@link IIrcIO}.
      */
     private final Map<IrcServer, IIrcIO> serverToIo = new HashMap<IrcServer, IIrcIO>();
-    
+
     /**
      * @param mtm
      */
     public IrcControl(ITriggerManager mtm) {
         super(mtm);
     }
-    
+
     /**
      * @see net.mauhiz.irc.base.IIrcClientControl#connect(IrcServer)
      */
@@ -47,7 +47,7 @@ public class IrcControl extends AbstractIrcControl implements IIrcClientControl 
             /* already connected */
             return;
         }
-        
+
         try {
             new IdentServer(server.getMyself()).start();
             IrcIO ircio = new IrcIO(this, server);
@@ -58,7 +58,7 @@ public class IrcControl extends AbstractIrcControl implements IIrcClientControl 
             LOG.error(e, e); // cannot connect
         }
     }
-    
+
     /**
      * @see net.mauhiz.irc.base.IIrcControl#exit()
      */
@@ -68,7 +68,7 @@ public class IrcControl extends AbstractIrcControl implements IIrcClientControl 
             io.disconnect();
         }
     }
-    
+
     public void quit(IrcServer server) {
         IIrcIO io = serverToIo.get(server);
         if (io != null) {
@@ -76,26 +76,32 @@ public class IrcControl extends AbstractIrcControl implements IIrcClientControl 
             serverToIo.remove(server);
         }
     }
-    
+
     /**
      * @see net.mauhiz.irc.base.IIrcControl#sendMsg(net.mauhiz.irc.base.msg.IIrcMessage)
      */
     public void sendMsg(IIrcMessage msg) {
-        IrcServer server = msg.getServer();
-        IIrcIO io = serverToIo.get(server);
-        if (io == null) {
-            throw new IllegalStateException("not connected to " + server.getAlias());
-        }
+        assert msg != null : "null msg";
         if (msg instanceof Privmsg) {
             if (StringUtils.isEmpty(((Privmsg) msg).getMessage())) {
                 LOG.info("Not sending empty msg", new IllegalArgumentException());
+                return;
             }
         } else if (msg instanceof Notice) {
             if (StringUtils.isEmpty(((Notice) msg).getMessage())) {
                 LOG.info("Not sending empty msg", new IllegalArgumentException());
+                return;
             }
         }
+
         LOG.info(msg);
+
+        IrcServer server = msg.getServer();
+        assert server != null : "no related server";
+
+        IIrcIO io = serverToIo.get(server);
+        assert io != null : "not connected to " + server.getAlias();
+
         io.sendMsg(msg.getIrcForm());
         if (msg instanceof Quit) {
             quit(server);
