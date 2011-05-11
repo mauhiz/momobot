a1659dafb8193db80cbadf2e02932e0bee2813e0
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2011-05-11 00:47:45 +0000

    codejam 2011 problems

---
+++ trunk/fbook_challenge/src/test/java/net/mauhiz/contest/codejam/qual2011/MagickaTest.java
@@ -0,0 +1,17 @@
+package net.mauhiz.contest.codejam.qual2011;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import net.mauhiz.contest.AbstractTester;
+
+public class MagickaTest extends AbstractTester {
+
+	@Test
+	@Override
+	public void doTest() throws IOException {
+		runTest(new Magicka());
+	}
+
+}
