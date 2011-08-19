02f16117d747ca5c716141fb8cbe36b63463a7e9
mauhiz <mauhiz@1e759161-072c-0410-a95b-f725d490f48e>
2011-08-19 05:21:31 +0000

    show unprocessed msgs

--- momobot_3/src/main/java/net/mauhiz/irc/gui/SwtIrcClient.java
+++ momobot_3/src/main/java/net/mauhiz/irc/gui/SwtIrcClient.java
@@ -237,6 +237,9 @@ public class SwtIrcClient {
         if (!processed) {
             processed = processServerLog(msg);
         }
+        if (!processed) {
+            LOG.error("Unprocessed msg: " + msg);
+        }
         //        }
     }
 }
